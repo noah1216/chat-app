335d97fd52bfa7a11247bde65a2beda194b7832a
noah1216 <76505928+noah1216@users.noreply.github.com>
2020-12-31 23:42:13 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,11 @@
 class MessagesController < ApplicationController
+  
   def index
     @message = Message.new
     @room = Room.find(params[:room_id]) 
+    @messages = @room.messages.includes(:user)
     # サイドバーからのroom.idがparamsのハッシュとして届く
+    # @room.messagesはルームに紐付いたメッセージを示す
   end
 
  
